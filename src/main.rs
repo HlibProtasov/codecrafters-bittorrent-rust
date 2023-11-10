d6ddbcf68d8dff7cabdbc8d71d3403d240b56fcf
holovko <andrew.golowko@gmail.com>
2023-11-10 10:23:51 +0200

    pass 2nd stage

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use serde_json;
 use std::env;
+use std::str::Chars;
 
 // Available if you need it!
 // use serde_bencode
@@ -7,15 +8,30 @@ use std::env;
 #[allow(dead_code)]
 fn decode_bencoded_value(encoded_value: &str) -> serde_json::Value {
     // If encoded_value starts with a digit, it's a number
-    if let Some((len, other)) = encoded_value.split_once(':')
+    if encoded_value.is_empty()
     {
-        if let Ok(number) = len.parse::<usize>() {
-
-            // Example: "5:hello" -> "hello"
-            let string = &other[..number];
-            return serde_json::Value::String(string.to_owned());
-        }
+        panic!("Unhandled encoded value: {}", encoded_value)
     }
+    match &encoded_value.chars().next().unwrap() {
+        'i' => {
+            let number = encoded_value[1..encoded_value.len() - 1].parse::<i64>().unwrap();
+            return serde_json::Value::Number(number.into());
+        }
+        '0'..='9' =>
+            {
+                if let Some((len, other)) = encoded_value.split_once(':')
+                {
+                    if let Ok(number) = len.parse::<usize>() {
+
+                        // Example: "5:hello" -> "hello"
+                        let string = &other[..number];
+                        return serde_json::Value::String(string.to_owned());
+                    }
+                }
+            }
+        _ => {}
+    }
+
 
     panic!("Unhandled encoded value: {}", encoded_value)
 }
