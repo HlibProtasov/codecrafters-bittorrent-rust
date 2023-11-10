4474aaf592ec458008f4d03713c3ad22cd098132
holovko <andrew.golowko@gmail.com>
2023-11-10 12:37:24 +0200

    pass 3nd stage

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,36 @@
 use serde_json;
 use std::env;
 
+use clap::builder::TypedValueParser;
+use serde_json::Value;
+
+enum ParseTy
+{
+    Num,
+    Str,
+}
+
 
 // Available if you need it!
 // use serde_bencode
-
-#[allow(dead_code)]
-fn decode_bencoded_value(encoded_value: &str) -> serde_json::Value {
-    // If encoded_value starts with a digit, it's a number
+fn decode(encoded_value: &str) -> (Value, &str)
+{
+    // i53e
     match &encoded_value.chars().next() {
         Some('i') =>
             {
-                let number = encoded_value[1..encoded_value.len() - 1].parse::<i64>().unwrap();
-                return serde_json::Value::Number(number.into());
+                let mut rest_str = "";
+                if let Some(number) = encoded_value
+                    .split_once('e')
+                    .and_then(|(num, rest)|
+                        {
+                            rest_str = rest;
+
+                            num[1..].parse::<i64>().ok()
+                        })
+                {
+                    return (number.into(), rest_str);
+                }
             }
         Some('0'..='9') =>
             {
@@ -22,15 +40,42 @@ fn decode_bencoded_value(encoded_value: &str) -> serde_json::Value {
 
                         // Example: "5:hello" -> "hello"
                         let string = &other[..number];
-                        return serde_json::Value::String(string.to_owned());
+                        return (string.into(), &other[number..]);
                     }
                 }
             }
+
         _ => {}
     }
-
-
     panic!("Unhandled encoded value: {}", encoded_value)
+}
+
+#[allow(dead_code)]
+fn decode_bencoded_value(encoded_value: &str) -> Value {
+    // If encoded_value starts with a digit, it's a number
+
+    match encoded_value.chars().next() {
+        Some('l') => {
+            let mut result = &encoded_value[1..];
+            let mut vec = Vec::new();
+            loop {
+                let (value, rest) = decode(result);
+                vec.push(value);
+                if rest.is_empty() || rest.chars().next().unwrap() == 'e'
+                {
+                    return vec.into();
+                }
+                result = rest;
+            }
+        }
+        Some(_) => {
+            let (value, _) = decode(encoded_value);
+            return value;
+        }
+        _ => {
+            panic!("Unhandled encoded value: {}", encoded_value)
+        }
+    }
 }
 
 // Usage: your_bittorrent.sh decode "<encoded_value>"
